2a7d833936bb58d200fdfde322854a210ffc4715
marianoAugusto <marianoparajon@gmail.com>
2022-06-11 17:01:42 -0500

    3-funciones

---
+++ 3-funciones.js
@@ -0,0 +1,51 @@
+/* Las funciones son procedimientos, 
+un conjunto de sentencias o pasos que realizarán una tarea 
+o cálculo con ciertos valores.*/
+
+/* Tenemos dos tipo de funciones en js, 
+function Declaration y function Expression */
+
+/* el nombre reservado de function 
+y parametros que recibira esa function */
+
+// function declaration Declarativas  
+function miFuncion() { 
+    return 3; 
+}
+
+miFuncion(); // mandamos llamar la funcion
+
+// function Expression (también conocidas como funciones anónimas)
+var miFuncion = function(a,b) {  
+    return a + b;
+}
+
+miFuncion(); // mandamos llamar la variable como funcion
+
+
+/* ============================= */
+
+
+function saludarEstudiante(estudiante) {
+    console.log(`Hola ${estudiante}`);  // template strings (Plantillas de cadena de texto)
+}
+
+
+function suma(a,b) {  // está funcion recibe 2 parámetros, que se convierten en un placeholder de valores que se pueden utilizar dentro de la función.
+    var resultado = a + b; 
+}
+
+// Return, cómo regresamos un valor con una function. 
+
+
+
+function suma(a,b) {
+    var resultado =  a + b; 
+    return resultado;
+}
+
+function suma(a,b) {
+    return a + b; 
+}
+
+suma(20, 30); 
